46e175153bcb6e362063a899c8ff0427cf5dc824
Frans Korhonen <frans.korhonen@rmcios.fi>
2021-06-06 08:29:35 +0300

    Fix cross compile installdir location variable passing
    To get module artifacts copied to the right artifacts

--- src/rmcios.mk
+++ src/rmcios.mk
@@ -2,7 +2,8 @@ MAKE?=make
 GCC?=gcc
 GENDATE?=
 DLLTOOL?=dlltool
-OUTPUT_PATH?=../
+MAKEFILEDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+INSTALLDIR?=${MAKEFILEDIR}/../
 NIDAQDEF?=NIDAQmx.def
 LJMDEF?=LabJackM.def
 export
@@ -13,36 +14,33 @@ gentime:
 	$(GENDATE)
 
 artifacts: all
-	${COPY} ../LICENSE $(OUTPUT_PATH)
-	${COPY_DIR} ../examples $(OUTPUT_PATH)
+	${COPY} ../LICENSE $(INSTALLDIR)
+	${COPY_DIR} ../examples $(INSTALLDIR)
 
 rmcios: gentime 
-	$(MAKE) -f rmcios-windows.mk
+	$(MAKE) -f rmcios-windows.mk OUTPUT_DIR=${INSTALLDIR}
 
 nidaqmx-module: gentime 
 	$(MAKE) -C RMCIOS-NI-DAQmx-module LINKDEF=${NIDAQDEF}
-	$(MAKE) -C RMCIOS-NI-DAQmx-module install
+	$(MAKE) -C RMCIOS-NI-DAQmx-module install INSTALLDIR=${INSTALLDIR}
 
 labjack-module: gentime
 	$(MAKE) -C RMCIOS-Labjack-module LINKDEF=${LJMDEF}
-	$(MAKE) -C RMCIOS-Labjack-module install
+	$(MAKE) -C RMCIOS-Labjack-module install INSTALLDIR=${INSTALLDIR}
 
 python-module: gentime
 	$(MAKE) -C RMCIOS-Python
-	$(MAKE) -C RMCIOS-Python install
+	$(MAKE) -C RMCIOS-Python install INSTALLDIR=${INSTALLDIR}
 
 base-module: gentime 
 	$(MAKE) -C RMCIOS-base-module
-	$(MAKE) -C RMCIOS-base-module install
+	$(MAKE) -C RMCIOS-base-module install INSTALLDIR=${INSTALLDIR}
 
 std-module: gentime 
 	$(MAKE) -C RMCIOS-std-module
-	$(MAKE) -C RMCIOS-std-module install
+	$(MAKE) -C RMCIOS-std-module install INSTALLDIR=${INSTALLDIR}
 
 windows-module: gentime 
 	$(MAKE) -C RMCIOS-windows-module
-	$(MAKE) -C RMCIOS-windows-module install
-
-run_program: gentime 
-	$(MAKE) -f run_program.mk
+	$(MAKE) -C RMCIOS-windows-module install INSTALLDIR=${INSTALLDIR}
 
